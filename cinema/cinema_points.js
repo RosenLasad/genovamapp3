10451c29dc3a14bab051865b4baf10814a0c00b4
Rosen Lasad <luxandro010@gmail.com>
2026-03-28 22:28:56 +0100

    Add files via upload

---
+++ cinema/cinema_points.js
@@ -0,0 +1,292 @@
+(function(){
+  window.CINEMA_POINTS = [
+  {
+    "lat": 44.408045870500715,
+    "lng": 8.921544524339781,
+    "name": "The Space Cinema - Porto Antico",
+    "desc": {
+      "it": "Cinema The Space Cinema: 10 sale",
+      "en": "The Space Cinema: 10 screens.",
+      "es": "Cine The Space Cinema: 10 salas.",
+      "fr": "Cinéma The Space Cinema : 10 salles.",
+      "ar": "سينما The Space Cinema: 10 قاعات عرض.",
+      "ru": "Кинотеатр The Space Cinema: 10 залов.",
+      "zh": "The Space Cinema 电影院：10 个影厅。",
+      "lij": "Cinêma The Space Cinema: 10 sæe."
+    },
+    "addr": "Via Magazzini del Cotone, 16128 Genova GE",
+    "url": "https://www.thespacecinema.it/al-cinema/genova",
+    "img": "cinema/cinema-space.jpg"
+  },
+  {
+    "lat": 44.4091892812888,
+    "lng": 8.934406362205761,
+    "name": "Circuito City",
+    "desc": {
+      "it": "Circuito City: 2 sale.",
+      "en": "Circuito City: 2 screens.",
+      "es": "Circuito City: 2 salas.",
+      "fr": "Circuito City : 2 salles.",
+      "ar": "Circuito City: قاعتان للعرض.",
+      "ru": "Circuito City: 2 зала.",
+      "zh": "Circuito City：2 个影厅。",
+      "lij": "Circuito City: 2 sæe."
+    },
+    "addr": "Vico Carmagnola, 9, 16123 Genova GE",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-city.jpg"
+  },
+  {
+    "lat": 44.40641915267746,
+    "lng": 8.942080631773601,
+    "name": "Circuito America",
+    "desc": {
+      "it": "Cinema America: 2 sale.",
+      "en": "Cinema America: 2 screens.",
+      "es": "Cine America: 2 salas.",
+      "fr": "Cinéma America : 2 salles.",
+      "ar": "سينما America: قاعتان للعرض.",
+      "ru": "Кинотеатр America: 2 зала.",
+      "zh": "America 电影院：2 个影厅。",
+      "lij": "Cinêma America: 2 sæe."
+    },
+    "addr": "V. Colombo, 11, 16121 Genova",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-america.jpg"
+  },
+  {
+    "lat": 44.43505111503336,
+    "lng": 8.891605863943562,
+    "name": "Albatros",
+    "desc": {
+      "it": "Cinema Albatros: 2 sale.",
+      "en": "Cinema Albatros: 2 screens.",
+      "es": "Cine Albatros: 2 salas.",
+      "fr": "Cinéma Albatros : 2 salles.",
+      "ar": "سينما Albatros: قاعتان للعرض.",
+      "ru": "Кинотеатр Albatros: 2 зала.",
+      "zh": "Albatros 电影院：2 个影厅。",
+      "lij": "Cinêma Albatros: 2 sæe."
+    },
+    "addr": "Via Roggerone 8, Rivarolo",
+    "url": "http://www.albatroscinema.it/",
+    "img": "cinema/cinema-albatros.jpg"
+  },
+  {
+    "lat": 44.40861554566093,
+    "lng": 8.932312120130382,
+    "name": "Circuito Ariston",
+    "desc": {
+      "it": "Cinema Ariston: 2 sale.",
+      "en": "Cinema Ariston: 2 screens.",
+      "es": "Cine Ariston: 2 salas.",
+      "fr": "Cinéma Ariston : 2 salles.",
+      "ar": "سينما Ariston: قاعتان للعرض.",
+      "ru": "Кинотеатр Ariston: 2 зала.",
+      "zh": "Ariston 电影院：2 个影厅。",
+      "lij": "Cinêma Ariston: 2 sæe."
+    },
+    "addr": "Vicolo San Matteo 14/r, 16123 Genova GE",
+    "url": "https://circuitocinemagenova.com/",
+    "img": "cinema/cinema-ariston.jpg"
+  },
+  {
+    "lat": 44.40232467996563,
+    "lng": 8.935590935865186,
+    "name": "Circuito Corallo",
+    "desc": {
+      "it": "Cinema Corallo: 2 sale.",
+      "en": "Cinema Corallo: 2 screens.",
+      "es": "Cine Corallo: 2 salas.",
+      "fr": "Cinéma Corallo : 2 salles.",
+      "ar": "سينما Corallo: قاعتان للعرض.",
+      "ru": "Кинотеатр Corallo: 2 зала.",
+      "zh": "Corallo 电影院：2 个影厅。",
+      "lij": "Cinêma Corallo: 2 sæe."
+    },
+    "addr": "Via Innocenzo IV 13/r, 16123 Genova GE",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-corallo.jpg"
+  },
+  {
+    "lat": 44.40240467512631,
+    "lng": 8.952897712054087,
+    "name": "Circuito Odeon",
+    "desc": {
+      "it": "Cinema Odeon: 2 sale.",
+      "en": "Cinema Odeon: 2 screens.",
+      "es": "Cine Odeon: 2 salas.",
+      "fr": "Cinéma Odeon : 2 salles.",
+      "ar": "سينما Odeon: قاعتان للعرض.",
+      "ru": "Кинотеатр Odeon: 2 зала.",
+      "zh": "Odeon 电影院：2 个影厅。",
+      "lij": "Cinêma Odeon: 2 sæe."
+    },
+    "addr": "Corso Buenos Aires 83/r",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-odeon.jpg"
+  },
+  {
+    "lat": 44.40985976808526,
+    "lng": 8.937011427522513,
+    "name": "Circuito Sivori",
+    "desc": {
+      "it": "Cinema Sivori: 2 sale + sala FilmClub.",
+      "en": "Cinema Sivori: 2 screens + FilmClub theatre.",
+      "es": "Cine Sivori: 2 salas + sala FilmClub.",
+      "fr": "Cinéma Sivori : 2 salles + salle FilmClub.",
+      "ar": "سينما Sivori: قاعتان للعرض + قاعة FilmClub.",
+      "ru": "Кинотеатр Sivori: 2 зала + зал FilmClub.",
+      "zh": "Sivori 电影院：2 个影厅，加一间 FilmClub 影厅。",
+      "lij": "Cinêma Sivori: 2 sæe + sæ FilmClub."
+    },
+    "addr": "Salita Santa Caterina 12, 16123 Genova GE",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-sivori.jpg"
+  },
+  {
+    "lat": 44.38756692585114,
+    "lng": 9.015382773091664,
+    "name": "Circuito San Pietro",
+    "desc": {
+      "it": "Cinema San Pietro: 1 sala.",
+      "en": "Cinema San Pietro: 1 screen.",
+      "es": "Cine San Pietro: 1 sala.",
+      "fr": "Cinéma San Pietro : 1 salle.",
+      "ar": "سينما San Pietro: قاعة عرض واحدة.",
+      "ru": "Кинотеатр San Pietro: 1 зал.",
+      "zh": "San Pietro 电影院：1 个影厅。",
+      "lij": "Cinêma San Pietro: 1 sæ."
+    },
+    "addr": "Piazza Frassinetti 10, Quinto al Mare",
+    "url": "http://www.circuitocinemagenova.com/",
+    "img": "cinema/cinema-pietro.jpg"
+  },
+  {
+    "lat": 44.41618732983783,
+    "lng": 8.887821436326805,
+    "name": "Club Amici del Cinema",
+    "desc": {
+      "it": "Cinema Club Amici del Cinema: 1 sala.",
+      "en": "Cinema Club Amici del Cinema: 1 screen.",
+      "es": "Cine Club Amici del Cinema: 1 sala.",
+      "fr": "Cinéma Club Amici del Cinema : 1 salle.",
+      "ar": "سينما Club Amici del Cinema: قاعة عرض واحدة.",
+      "ru": "Кинотеатр Club Amici del Cinema: 1 зал.",
+      "zh": "Club Amici del Cinema 电影院：1 个影厅。",
+      "lij": "Cinêma Club Amici do Cinêma: 1 sæ."
+    },
+    "addr": "Via Carlo Rolando 15, Sampierdarena",
+    "url": "http://www.clubamicidelcinema.it/",
+    "img": "cinema/cinema-amici.jpg"
+  },
+  {
+    "lat": 44.417891934952046,
+    "lng": 8.939309609778695,
+    "name": "Cineclub Fritz Lang",
+    "desc": {
+      "it": "Cinema Fritz Lang: 1 sala.",
+      "en": "Cinema Fritz Lang: 1 screen.",
+      "es": "Cine Fritz Lang: 1 sala.",
+      "fr": "Cinéma Fritz Lang : 1 salle.",
+      "ar": "سينما Fritz Lang: قاعة عرض واحدة.",
+      "ru": "Кинотеатр Fritz Lang: 1 зал.",
+      "zh": "Fritz Lang 电影院：1 个影厅。",
+      "lij": "Cinêma Fritz Lang: 1 sæ."
+    },
+    "addr": "Via Acquarone 64/r, Castelletto",
+    "url": "http://www.fritzlang.it/",
+    "img": "cinema/cinema-fritz.jpg"
+  },
+  {
+    "lat": 44.42774244127006,
+    "lng": 8.774844964033285,
+    "name": "Nuovo Cinema Palmaro",
+    "desc": {
+      "it": "Nuovo Cinema Palmaro: 1 sala.",
+      "en": "Nuovo Cinema Palmaro: 1 screen.",
+      "es": "Nuovo Cinema Palmaro: 1 sala.",
+      "fr": "Nuovo Cinema Palmaro : 1 salle.",
+      "ar": "Nuovo Cinema Palmaro: قاعة عرض واحدة.",
+      "ru": "Nuovo Cinema Palmaro: 1 зал.",
+      "zh": "Nuovo Cinema Palmaro：1 个影厅。",
+      "lij": "Nêuvo Cinêma Palmaro: 1 sæ."
+    },
+    "addr": "Via Prà 164, Palmaro",
+    "url": "http://www.nuovocinemapalmaro.it/",
+    "img": "cinema/cinema-palmaro.jpg"
+  },
+  {
+    "lat": 44.40621273080449,
+    "lng": 8.941334101910837,
+    "name": "Nickelodeon",
+    "desc": {
+      "it": "Cinema Nickelodeon: 1 sala.",
+      "en": "Cinema Nickelodeon: 1 screen.",
+      "es": "Cine Nickelodeon: 1 sala.",
+      "fr": "Cinéma Nickelodeon : 1 salle.",
+      "ar": "سينما Nickelodeon: قاعة عرض واحدة.",
+      "ru": "Кинотеатр Nickelodeon: 1 зал.",
+      "zh": "Nickelodeon 电影院：1 个影厅。",
+      "lij": "Cinêma Nickelodeon: 1 sæ."
+    },
+    "addr": "Via della Consolazione 1, traversa di Via XX Settembre",
+    "url": "http://www.cineclubnickelodeon.it/",
+    "img": "cinema/cinema-nick.jpg"
+  },
+  {
+    "lat": 44.413676980060956,
+    "lng": 8.880831287763659,
+    "name": "UCI Luxe Fiumara",
+    "desc": {
+      "it": "Cinema UCI Luxe Fiumara: 9 sale + sala IMAX.",
+      "en": "Cinema UCI Luxe Fiumara: 9 screens + IMAX theatre.",
+      "es": "Cine UCI Luxe Fiumara: 9 salas + sala IMAX.",
+      "fr": "Cinéma UCI Luxe Fiumara : 9 salles + salle IMAX.",
+      "ar": "سينما UCI Luxe Fiumara: 9 قاعات عرض + قاعة IMAX.",
+      "ru": "Кинотеатр UCI Luxe Fiumara: 9 залов + зал IMAX.",
+      "zh": "UCI Luxe Fiumara 电影院：9 个影厅，加一间 IMAX 影厅。",
+      "lij": "Cinêma UCI Luxe Fiumara: 9 sæe + sæ IMAX."
+    },
+    "addr": "Via Mantovani, centro commerciale Fiumara, Sampierdarena",
+    "url": "https://ucicinemas.it/cinema/uci-cinemas-fiumara-genova",
+    "img": "cinema/cinema-uci.jpg"
+  },
+  {
+    "lat": 44.426299324586346,
+    "lng": 8.849963502834518,
+    "name": "Cinema Verdi",
+    "desc": {
+      "it": "Cinema Verdi: 2 sale.",
+      "en": "Cinema Verdi: 2 screens.",
+      "es": "Cine Verdi: 2 salas.",
+      "fr": "Cinéma Verdi : 2 salles.",
+      "ar": "سينما Verdi: قاعتان للعرض.",
+      "ru": "Кинотеатр Verdi: 2 зала.",
+      "zh": "Verdi 电影院：2 个影厅。",
+      "lij": "Cinêma Verdi: 2 sæe."
+    },
+    "addr": "Piazza Oriani 7, Sestri Ponente",
+    "url": "http://verditeatro.it/",
+    "img": "cinema/cinema-verdi.jpg"
+  },
+  {
+    "lat": 44.38421431599317,
+    "lng": 9.038812900328725,
+    "name": "San Siro Nervicinema",
+    "desc": {
+      "it": "Cinema San Siro: 1 sala all'aperto.",
+      "en": "Cinema San Siro: 1 open-air screen.",
+      "es": "Cine San Siro: 1 sala al aire libre.",
+      "fr": "Cinéma San Siro : 1 salle en plein air.",
+      "ar": "سينما San Siro: قاعة عرض واحدة في الهواء الطلق.",
+      "ru": "Кинотеатр San Siro: 1 зал под открытым небом.",
+      "zh": "San Siro 电影院：1 个露天影厅.",
+      "lij": "Cinêma San Siro: 1 sæ fœa."
+    },
+    "addr": "Via alla Chiesa Plebana 15, Nervi",
+    "url": "http://www.sansironervi.org/cinema.html",
+    "img": "cinema/cinema-siro.jpg"
+  }
+];
+})();
